6f800360f7c02a09905f218473a62b402b850d0c
Keshav <keshavjha0610@gmail.com>
2026-05-10 13:19:15 +0530

    feat: add demonstration script for JavaScript typeof operator behavior

---
+++ typeof.js
@@ -0,0 +1,19 @@
+let x = 30;
+let str = "Hello";
+let bool = true;
+let und = undefined;
+let nul = null;
+let arr = [1, 2, 3];
+let obj = { name: "John" };
+let func = function () { };
+let date = new Date();
+
+console.log(typeof x); // Number
+console.log(typeof str); // String
+console.log(typeof bool); // Boolean
+console.log(typeof und); // undefined
+console.log(typeof nul); // Object
+console.log(typeof arr); // Object
+console.log(typeof obj); // Object
+console.log(typeof func); // Function
+console.log(typeof date); // Object
